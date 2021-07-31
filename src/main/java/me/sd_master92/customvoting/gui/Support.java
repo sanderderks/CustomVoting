ee061f32561fa797da61edae4d5a6bd01a1a7633
Sander Derks <sander1derks@live.nl>
2021-07-31 19:56:04 +0200

    Update Support.java

--- src/main/java/me/sd_master92/customvoting/gui/Support.java
+++ src/main/java/me/sd_master92/customvoting/gui/Support.java
@@ -27,6 +27,9 @@ public class Support extends GUI
         getInventory().setItem(1, Settings.getDoIngameUpdatesSetting(plugin));
         getInventory().setItem(2, createItem(Material.ENCHANTED_BOOK, ChatColor.LIGHT_PURPLE + "Discord",
                 ChatColor.GRAY + "Join the discord server"));
+        getInventory().setItem(3, createItem(Material.ENCHANTING_TABLE, ChatColor.LIGHT_PURPLE + "Database",
+                ChatColor.GRAY + "Status: " + (plugin.useDatabase() ? ChatColor.GREEN + "Connected" :
+                        ChatColor.RED + "Disabled")));
         getInventory().setItem(8, BACK_ITEM);
     }
 
