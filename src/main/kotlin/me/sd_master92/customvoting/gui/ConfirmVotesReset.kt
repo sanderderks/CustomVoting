17f4d2f1538a94550c26662600084610bf9caaba
Sander Derks <sander1derks@live.nl>
2022-01-27 14:54:15 +0100

    Added confirm reset dialog

---
+++ src/main/kotlin/me/sd_master92/customvoting/gui/ConfirmVotesReset.kt
@@ -0,0 +1,53 @@
+package me.sd_master92.customvoting.gui
+
+import me.sd_master92.customfile.PlayerFile.Companion.getAll
+import me.sd_master92.customvoting.CV
+import me.sd_master92.customvoting.VoteFile
+import me.sd_master92.customvoting.broadcastText
+import me.sd_master92.customvoting.constants.Messages
+import me.sd_master92.customvoting.database.PlayerRow
+import me.sd_master92.customvoting.tasks.DailyTask
+import org.bukkit.ChatColor
+import org.bukkit.entity.Player
+import org.bukkit.event.inventory.InventoryClickEvent
+import kotlin.jvm.internal.Intrinsics
+
+
+class ConfirmVotesReset(private val plugin: CV, private val period: Boolean) : ConfirmGUI(plugin, if (period) "Reset monthly votes?" else "Reset ALL votes?")
+{
+    override fun onConfirm(event: InventoryClickEvent, player: Player)
+    {
+        Intrinsics.checkNotNullParameter(event, "event")
+        Intrinsics.checkNotNullParameter(player, "player")
+        for ((_, value) in getAll())
+        {
+            if (plugin.hasDatabaseConnection())
+            {
+                if (period)
+                {
+                    PlayerRow(plugin, value.uuid).clearPeriod()
+                } else
+                {
+                    PlayerRow(plugin, value.uuid).setVotes(0, true)
+                }
+            }
+            if (period)
+            {
+                VoteFile(value.uuid, plugin).clearPeriod()
+            } else
+            {
+                VoteFile(value.uuid, plugin).setVotes(0, true)
+            }
+        }
+        DailyTask.FIRST_OF_MONTH = false
+        broadcastText(plugin, Messages.MONTHLY_RESET)
+        player.openInventory(VoteSettings(plugin).inventory)
+    }
+
+    override fun onCancel(event: InventoryClickEvent, player: Player)
+    {
+        DailyTask.FIRST_OF_MONTH = false
+        player.sendMessage(Intrinsics.stringPlus(ChatColor.RED.toString(), "Votes are not reset."))
+        player.openInventory(VoteSettings(plugin).inventory)
+    }
+}
--- src/main/kotlin/me/sd_master92/customvoting/tasks/DailyTask.kt
+++ src/main/kotlin/me/sd_master92/customvoting/tasks/DailyTask.kt
@@ -1,59 +1,17 @@
 package me.sd_master92.customvoting.tasks
 
-import me.sd_master92.customfile.PlayerFile
 import me.sd_master92.customvoting.CV
-import me.sd_master92.customvoting.VoteFile
 import me.sd_master92.customvoting.commands.ReloadCommand
-import me.sd_master92.customvoting.constants.Messages
-import me.sd_master92.customvoting.constants.Settings
-import me.sd_master92.customvoting.database.PlayerRow
 import org.bukkit.scheduler.BukkitRunnable
 import java.util.*
 
 class DailyTask(private val plugin: CV)
 {
-    private fun checkMonthlyReset()
+    companion object
     {
-        if (plugin.config.getBoolean(Settings.MONTHLY_RESET))
-        {
-            if (Calendar.getInstance()[Calendar.DAY_OF_MONTH] == 1)
-            {
-                for (playerFile in PlayerFile.getAll())
-                {
-                    if (plugin.hasDatabaseConnection())
-                    {
-                        PlayerRow(plugin, playerFile.value.uuid).setVotes(0, true)
-                    } else
-                    {
-                        VoteFile(playerFile.value.uuid, plugin).setVotes(0, true)
-                    }
-                }
-                plugin.server.broadcastMessage(Messages.MONTHLY_RESET.getMessage(plugin))
-            }
-        }
+        var FIRST_OF_MONTH = false
     }
-
-    private fun checkMonthlyPeriod()
-    {
-        if (plugin.config.getBoolean(Settings.MONTHLY_PERIOD))
-        {
-            if (Calendar.getInstance()[Calendar.DAY_OF_MONTH] == 1)
-            {
-                for (playerFile in PlayerFile.getAll())
-                {
-                    if (plugin.hasDatabaseConnection())
-                    {
-                        PlayerRow(plugin, playerFile.value.uuid).clearPeriod()
-                    } else
-                    {
-                        VoteFile(playerFile.value.uuid, plugin).clearPeriod()
-                    }
-                }
-                plugin.server.broadcastMessage(Messages.MONTHLY_RESET.getMessage(plugin))
-            }
-        }
-    }
-
+    
     init
     {
         object : BukkitRunnable()
@@ -61,8 +19,10 @@ class DailyTask(private val plugin: CV)
             override fun run()
             {
                 ReloadCommand.reload(plugin)
-                checkMonthlyReset()
-                checkMonthlyPeriod()
+                if (Calendar.getInstance()[Calendar.DAY_OF_MONTH] == 1)
+                {
+                    FIRST_OF_MONTH = true
+                }
             }
         }.runTaskTimer(plugin, 60, (20 * 60 * 60).toLong())
     }
