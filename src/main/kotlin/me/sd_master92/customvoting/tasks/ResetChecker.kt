8c611d0e94379ffc90a4cd1ca6edec673052601d
Sander Derks <sander1derks@live.nl>
2023-05-21 21:01:06 +0200

    Monthly,Weekly and Daily Votes are now reset automatically, checked hourly

--- src/main/kotlin/me/sd_master92/customvoting/tasks/ResetChecker.kt
+++ src/main/kotlin/me/sd_master92/customvoting/tasks/ResetChecker.kt
@@ -2,36 +2,44 @@ package me.sd_master92.customvoting.tasks
 
 import me.sd_master92.core.tasks.TaskTimer
 import me.sd_master92.customvoting.CV
-import me.sd_master92.customvoting.commands.ReloadCommand
-import me.sd_master92.customvoting.constants.enumerations.PMessage
-import me.sd_master92.customvoting.constants.enumerations.Setting
-import org.bukkit.Bukkit
+import me.sd_master92.customvoting.constants.interfaces.Voter
 import java.util.*
 
 class ResetChecker(private val plugin: CV)
 {
-    companion object
-    {
-        var FIRST_OF_MONTH = false
-    }
+    private var lastResetDay: Int = -1
+    private var lastResetWeek: Int = -1
+    private var lastResetMonth: Int = -1
 
     init
     {
-        TaskTimer.repeat(plugin, 20 * 60 * 60, 20 * 3)
-        {
-            ReloadCommand.reload(plugin)
-            if (Calendar.getInstance()[Calendar.DAY_OF_MONTH] == 1)
+        TaskTimer.repeat(plugin, 20 * 60 * 60, 20 * 3) {
+            val calendar = Calendar.getInstance()
+
+            if (calendar[Calendar.MONTH] != lastResetMonth)
             {
-                FIRST_OF_MONTH = true
-                if (plugin.config.getBoolean(Setting.MONTHLY_RESET.path))
+                lastResetMonth = calendar[Calendar.MONTH]
+                for (voter in Voter.getTopVoters(plugin))
                 {
-                    for (player in Bukkit.getOnlinePlayers())
-                    {
-                        if (player.isOp)
-                        {
-                            player.sendMessage(PMessage.RESET_VOTES_MESSAGE_CONFIRM.toString())
-                        }
-                    }
+                    voter.clearMonthlyVotes()
+                }
+            }
+
+            if (calendar[Calendar.WEEK_OF_YEAR] != lastResetWeek)
+            {
+                lastResetWeek = calendar[Calendar.WEEK_OF_YEAR]
+                for (voter in Voter.getTopVoters(plugin))
+                {
+                    voter.clearWeeklyVotes()
+                }
+            }
+
+            if (calendar[Calendar.DAY_OF_MONTH] != lastResetDay)
+            {
+                lastResetDay = calendar[Calendar.DAY_OF_MONTH]
+                for (voter in Voter.getTopVoters(plugin))
+                {
+                    voter.clearDailyVotes()
                 }
             }
         }.run()
