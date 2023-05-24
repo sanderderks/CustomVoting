6579dd7459ad63d05c10806ac2b695b1bda0ae9c
Sander Derks <sander1derks@live.nl>
2023-05-24 06:46:10 +0200

    Stabilized vote top npc's + changed entity id to citizens id

--- src/main/kotlin/me/sd_master92/customvoting/subjects/stands/Citizen.kt
+++ src/main/kotlin/me/sd_master92/customvoting/subjects/stands/Citizen.kt
@@ -2,47 +2,68 @@ package me.sd_master92.customvoting.subjects.stands
 
 import me.sd_master92.customvoting.CV
 import me.sd_master92.customvoting.constants.enumerations.ArmorType
-import me.sd_master92.customvoting.constants.enumerations.PMessage
 import net.citizensnpcs.api.CitizensAPI
 import net.citizensnpcs.api.npc.NPC
 import net.citizensnpcs.api.trait.trait.Equipment
 import org.bukkit.Location
 import org.bukkit.entity.EntityType
-import java.util.*
 
-class Citizen(private val plugin: CV, private val path: String, private val top: Int, private val loc: Location?)
+class Citizen(private val plugin: CV, private val path: String, private val top: Int, private var loc: Location?)
 {
     private var npc: NPC? = null
 
-    fun update(name: String)
+    private fun refresh()
     {
-        if (CV.CITIZENS)
+        npc?.let { npc ->
+            npc.data().setPersistent(NPC.Metadata.NAMEPLATE_VISIBLE, false)
+            if (npc.isSpawned) npc.despawn()
+            loc?.let { npc.spawn(it) }
+        }
+    }
+
+    private fun findOrCreate()
+    {
+        if (npc == null)
         {
-            npc?.let {
-                if (it.name != name)
-                {
-                    it.name = name
-                    ArmorType.dress(it.getOrAddTrait(Equipment::class.java), top)
-                    refresh()
-                }
-            } ?: run {
-                findOrCreate()
+            val id = plugin.data.getNumber(path)
+            npc = if (id > 0)
+            {
+                CitizensAPI.getNPCRegistry().getById(id)
+            } else
+            {
+                create(loc)
+            }
+        }
+        npc?.let { npc ->
+            if (!npc.isSpawned && loc != null)
+            {
+                npc.spawn(loc)
             }
         }
     }
 
-    fun create(loc: Location)
+    fun create(newLoc: Location?): NPC?
     {
-        if (CV.CITIZENS)
-        {
-            npc = CitizensAPI.getNPCRegistry().createNPC(EntityType.PLAYER, PMessage.PLAYER_NAME_UNKNOWN.toString())
-            npc?.let {
-                it.spawn(loc)
-                ArmorType.dress(it.getOrAddTrait(Equipment::class.java), top)
-                plugin.data.set(path, it.uniqueId.toString())
-                plugin.data.saveConfig()
+        this.loc = newLoc
+        if (loc == null) return null
+        val newNpc = CitizensAPI.getNPCRegistry().createNPC(EntityType.PLAYER, "Steve")
+        newNpc.spawn(loc)
+        ArmorType.dress(newNpc.getOrAddTrait(Equipment::class.java), top)
+        plugin.data.setNumber(path, newNpc.id)
+        return newNpc
+    }
+
+    fun update(name: String)
+    {
+        if (npc == null) findOrCreate()
+        npc?.let { npc ->
+            if (npc.name != name)
+            {
+                npc.name = name
+                ArmorType.dress(npc.getOrAddTrait(Equipment::class.java), top)
             }
         }
+        refresh()
     }
 
     fun delete()
@@ -51,41 +72,13 @@ class Citizen(private val plugin: CV, private val path: String, private val top:
         plugin.data.delete(path)
     }
 
-    private fun findOrCreate()
-    {
-        val uuid = plugin.data.getString(path)
-        npc = if (uuid != null)
-        {
-            CitizensAPI.getNPCRegistry().getByUniqueId(UUID.fromString(uuid))
-        } else
-        {
-            create(loc!!)
-            return
-        }
-        refresh()
-    }
-
-    private fun refresh()
-    {
-        npc?.let {
-            if (it.isSpawned)
-            {
-                val loc = it.entity.location
-                it.despawn()
-                it.spawn(loc)
-            } else if (loc != null)
-            {
-                it.spawn(loc)
-            }
-            it.data().setPersistent(NPC.Metadata.NAMEPLATE_VISIBLE, false)
-        }
-    }
-
     init
     {
         if (CV.CITIZENS)
         {
             findOrCreate()
+            refresh()
         }
     }
 }
+
